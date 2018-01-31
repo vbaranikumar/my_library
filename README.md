38d0bf46b6482642079008164db20c2fc6ddd511
Barani Kumar <vbaranikumar@gmail.com>
2018-01-31 09:12:58 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# my_library
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: my_library
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
